0d468fe09494b258ebafd1320005a6622a9937df
enesssari <enes.sari@boun.edu.tr>
2022-07-02 15:40:20 +0300

    New Web Api Trial

---
+++ AdessoChallange-WebApplication1/AdessoChallange-WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdessoChallange_WebApplication1.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+       
+        public IActionResult Get()
+        {
+            return Ok("Hello World!");
+        }
+
+    }
+}
